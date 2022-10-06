4769c755efc372a5afbbc396e6a7aa253a4d7412
Stacia <staciagreen.1@mail.ru>
2022-10-07 00:13:25 +0300

    input

--- grinko_367172/main.cpp
+++ grinko_367172/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+    int a;
+    std::string sign;
+    int b;
+    std::cin >> a >> sign >> b;
     std::cout << "Hello, this is SimpleCalc!" << std::endl;
     return 0;
 }
